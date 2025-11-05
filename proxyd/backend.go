0e1070830a0eba01751f7a71f4bbd4ffdbbfb84a
Javier Cortejoso <javier.cortejoso@gmail.com>
2025-11-05 12:04:26 +0100

    Enhance error detection in `containsArchiveRequiredError` function
    - Added checks for new error messages related to data availability in the `containsArchiveRequiredError` function to improve error handling for RPC responses.

--- proxyd/backend.go
+++ proxyd/backend.go
@@ -1831,7 +1831,9 @@ func containsArchiveRequiredError(responses []*RPCRes) bool {
 				strings.Contains(res.Error.Message, "distance to target block exceeds maximum proof window") ||
 				strings.Contains(res.Error.Data, "distance to target block exceeds maximum proof window") ||
 				strings.Contains(res.Error.Message, "No state available for block") ||
-				strings.Contains(res.Error.Data, "No state available for block") {
+				strings.Contains(res.Error.Data, "No state available for block") ||
+				strings.Contains(res.Error.Message, "data before") && strings.Contains(res.Error.Message, "not available") ||
+				strings.Contains(res.Error.Data, "data before") && strings.Contains(res.Error.Data, "not available") {
 				return true
 			}
 		}
